c266d95322709e762ca9126d71277063ec92c481
ohyoungjooung2 <wnapdlf05@gmail.com>
2014-12-24 16:13:27 +0900

    removed apc apt

--- apc/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'apc'
-maintainer       'Your Company, Inc.'
-maintainer_email 'cookbooks@yourcompany.com'
-license          'Apache 2.0'
-description      'Installs/Configures apc'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
