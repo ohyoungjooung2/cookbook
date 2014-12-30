9c77a8b2ab99dff7b053b391e089111f4488efea
ohyoungjooung2 <wnapdlf05@gmail.com>
2014-12-31 02:49:49 +0900

    jenkins modified multiple include

--- jenkins/recipes/default.rb
+++ jenkins/recipes/default.rb
@@ -16,8 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-#include_recipe "tomcat"
+include_recipe "java"
+include_recipe "tomcat"
 
 tu=node[:tomcat][:user] 
 tg=node[:tomcat][:group]
